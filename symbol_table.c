540f65ca3726b5306d905bf6e5cae43b30afc3f8
Cameron Lee <cameronslee@hotmail.com>
2023-11-01 22:30:04 -0700

    symbol table stable

--- symbol_table.c
+++ symbol_table.c
@@ -1,28 +1,30 @@
 #include "symbol_table.h"
 
-int lookup(symtable_T *t, char *lexeme) {
+int lookup(symtable_T *t, char lexeme[]) {
   int i;
-  for (i = t->last_pos; i >= 0; i--) {
+  for (i = t->last_pos; i > 0; i--) {
     if (strcmp(t->table[i].value, lexeme) == 0) return i;
   }
   return 0;
 }
 
-int insert(symtable_T *t, char *lexeme, token_name_T type) {
+int insert(symtable_T *t, char lexeme[], token_name_T type) {
   int len;
-  size_t new_size;
+  size_t new_size = 0;
   len = strlen(lexeme);
-  if (t->last_pos + 1 >= t->size) {
+  if ((unsigned long)(t->last_pos + 1) >= t->capacity) {
     // TODO for now, just double the size of the table
-    new_size = (t->size * 2) * sizeof(token_T);
+    new_size = (t->capacity* 2) * sizeof(token_T);
     t = realloc(t, new_size); 
   }
 
   t->last_pos += 1;
+  t->size += 1;
+  t->table[t->last_pos].value = malloc(sizeof(char) * len);
   t->table[t->last_pos].type = type;
-  t->table[t->last_pos].value = lexeme;
 
   strncpy(t->table[t->last_pos].value, lexeme, len);
+
   return t->last_pos;
 }
 
@@ -30,12 +32,10 @@ symtable_T *init_symtable(size_t initial_size) {
   symtable_T *symtable = malloc(sizeof(symtable_T));
   symtable->table = malloc(sizeof(token_T) * initial_size);
   symtable->last_pos = 0;
-  symtable->size = initial_size;
+  symtable->size = 0;
+  symtable->capacity= initial_size;
   
   //load keywords
-  char *foo;
-  foo = "int";
-  insert(symtable, foo, KEYWORD);
-
+  insert(symtable, "int", KEYWORD);
   return symtable;
 }
--- symbol_table.h
+++ symbol_table.h
@@ -10,18 +10,19 @@
 #include "token.h"
 
 typedef struct {
-  token_T *table;
   size_t size;
-  unsigned int last_pos; // last used pos in symtable
+  size_t capacity;
+  int last_pos; // last used pos in symtable
+  token_T *table;
 } symtable_T;
 
 // returns the index of the lexeme in the symtable. 
 // returns 0 if it does not exist
-int lookup(symtable_T *table, char *lexeme); 
+int lookup(symtable_T *table, char lexeme[]); 
 
 // creates a token and adds it to the symtable
 // returns the position of entry of the lexeme
-int insert(symtable_T *table, char *lexeme, token_name_T type);  
+int insert(symtable_T *table, char lexeme[], token_name_T type);  
 
 // init and load with keywords
 symtable_T * init_symtable(size_t initial_size);
