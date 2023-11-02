1e230278ef69f9d2eb5e7924bc941226785b5e36
Cameron Lee <cameronslee@hotmail.com>
2023-11-01 22:22:41 -0700

    identifier and int

--- token.h
+++ token.h
@@ -1,19 +1,28 @@
 #ifndef TOKEN_H
 #define TOKEN_H 
 
+#define DONE 69420 
+
 typedef enum {
-  IDENTIFIER,
-  KEYWORD,
-  SEPARATOR,
-  OPERATOR,
-  LITERAL,
-  COMMENT
-} TokenName;
+  IDENTIFIER, // alphanum followed by num || alphanum
+  KEYWORD, // keywords loaded into symtable at start
+  SEPARATOR, //TODO implement {, }, (, ), ;
+  OPERATOR, // TODO implement arith ops
+  LITERAL, // TODO for now, just a sequence of digits
+  NONE,
+} token_name_T;
 
 typedef struct {
-  TokenName type;
-  char *value;
+  token_name_T type;
+
+  /* Attribute value usage
+  // literals are stored as they are.
+  // identifiers stored as the index into the sym table
+  */
+  char *value; // attribute value
 } token_T;
+
+const char * get_token_name(token_name_T type);
 
 #endif
 
